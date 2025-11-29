2f970e2a200c3f921eb3432ed9b81b7c405a8640
lalala_233 <lalala_233@qq.com>
2025-11-29 20:32:13 +0800

    feat: create multiple window

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,40 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/WindowEnums.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+using sf::CircleShape;
+using sf::Color;
+using sf::Event;
+using sf::RenderWindow;
+using sf::VideoMode;
+using std::optional;
+#define WINDOW_SIZE 200
+int main() {
+    RenderWindow windows[5];
+    for (int i = 0; i < 5; i++) {
+        windows[i].create(
+            VideoMode({WINDOW_SIZE, WINDOW_SIZE}),
+            "SFML works!",
+            sf::Style::Titlebar | sf::Style::Close
+        );
+        windows[i].setFramerateLimit(10);
+        windows[i].setPosition({i * WINDOW_SIZE, 0});
+    }
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
+    CircleShape shape(100.f);
+    shape.setFillColor(Color::Green);
+    // window.setVerticalSyncEnabled(true);
+    while (windows[0].isOpen()) {
+        for (auto& window: windows) {
+            if (window.isOpen()) {
+                while (const optional event = window.pollEvent()) {
+                    if (event->is<Event::Closed>()) {
+                        window.close();
+                    }
+                }
+                window.clear();
+                window.draw(shape);
+                window.display();
+            }
         }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
     }
 }
